957ba33b9e24e92abf941c4385e4a24ec9549192
Samuel Fox <samueljasonfox@gmail.com>
2021-08-21 17:11:18 -0700

    update license layout

--- utils/licenses/index.js
+++ utils/licenses/index.js
@@ -1,7 +1,7 @@
 const { name } = require('../config/config')
 
 const licenses = {
-    mit: (year) => `MIT License
+    mit: (year) => `# [![license](https://img.shields.io/badge/License-MIT-blue)](#License)
 
 Copyright ${year} ${name}
 
@@ -11,7 +11,7 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 `,
-    none: () => 'n/a'
+    none: (year) => `# License\n\nno license available\n\n${year}`
 }
 
 module.exports = licenses
